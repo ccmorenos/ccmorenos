65d05922725e453f67767011752b0e563f8d4e93
Catalina Moreno <ccatamorenos@gmail.com>
2022-09-13 23:36:27 -0500

    Add animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,12 +4,45 @@ module.exports = {
     theme: {
         extend: {
             animation: {
-                fadeIn: "fadeIn 1s ease-in forwards"
+                fadeIn: "fadeIn 1s ease-in forwards",
+                moveRi: "moveRi 1s ease-in forwards",
+                moveLe: "moveLe 1s ease-in forwards",
+                moveUp: "moveUp 0.7s ease-in forwards"
             },
             keyframes: {
                 fadeIn: {
                     "0%": { opacity: 0 },
                     "100%": { opacity: 1 }
+                },
+                moveRi: {
+                    "0%": {
+                        opacity: 0 ,
+                        transform: "translate(-100px)"
+                    },
+                    "100%": {
+                        opacity: 1,
+                        transform: "translate(0px)"
+                    }
+                },
+                moveLe: {
+                    "0%": {
+                        opacity: 0 ,
+                        transform: "translate(100px)"
+                    },
+                    "100%": {
+                        opacity: 1,
+                        transform: "translate(0px)"
+                    }
+                },
+                moveUp: {
+                    "0%": {
+                        opacity: 0 ,
+                        transform: "translateY(50px)"
+                    },
+                    "100%": {
+                        opacity: 1,
+                        transform: "translateY(0px)"
+                    }
                 }
             },
             variants: {
